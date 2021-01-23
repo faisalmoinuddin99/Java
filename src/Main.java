c4ed767fba5eb1d785a0031c6800d0d073786661
faisal <faisal25march99@gmail.com>
2021-01-23 11:09:42 +0530

    hasNextByte(), next(), nextLine()

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,13 @@ public class Main {
       Scanner input = new Scanner(System.in) ; // Scanner class
       System.out.println("Enter the age of user");
       byte number = input.nextByte() ; // 25
+      boolean b1 = input.hasNextByte(); // its use to check authenticity of variable
+      System.out.println(b1);
       System.out.println("The age of user " + number); // 25
+
+      String str = input.next() ; // its only pick one word
+      String strLine = input.nextLine(); // it prints the sentences
+      System.out.println(strLine);
     }
 }
 
