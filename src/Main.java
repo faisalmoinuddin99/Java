12558754d740c501378fa9b2aa9dd53ac1b34475
faisal <faisal25march99@gmail.com>
2021-01-23 07:27:16 +0530

    Variables and Data types, Done with Quiz and also used Math.Pow function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+      // Quiz WAP to add three number
+
+        int num1 = 60;
+        int num2 = 2 ;
+        int num3 = 6 ;
+
+        System.out.println(num1 + num2 + num3);
+
+        double resultOfInt = -(Math.pow(2,8) / 2 )  ;
+        System.out.println(resultOfInt);
     }
 }
 
