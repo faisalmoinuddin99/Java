7dbd35db763139ee689411d63aaead80da10fc33
faisal <faisal25march99@gmail.com>
2021-01-23 18:01:32 +0530

    Comparison Operator

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Assignment Operators
-        int a = 6 ;
-        int b = a + 4 ;
-        b += 7 ;
-        b *= 50 ;
-        b %= 4 ; // Modulo operator
-        System.out.println(b);
+        // comparison operator
+        System.out.println(64 > 6);
+        System.out.println(6 == 9);
+        System.out.println(5 != 0);
 
     }
 }
