50ba99b47db27804f5c78e242131493c4ff218b7
faisal <faisal25march99@gmail.com>
2021-01-23 18:03:58 +0530

    Logical Operator

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // comparison operator
-        System.out.println(64 > 6);
-        System.out.println(6 == 9);
-        System.out.println(5 != 0);
+       // Logical Operator
+        System.out.println(65 > 7 && 0 != 1);
+        System.out.println(54 == 45 || 5 == 5 );
 
     }
 }
