eb530cb148c905919d5ef4ed497aedf2999764d2
faisal <faisal25march99@gmail.com>
2021-01-23 12:02:16 +0530

    Done with Question 4 Kilometer to Miles

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
       /*
-       * WAP which ask the user to enter his/her name and greets them with "Hello <name>, have a good day " text
+       * WAP to convert kilometer to miles:
+       *
+       *  Algorithm :
+       *  step1 : Take a kilometer value from user
+       *  step2 : multiple kilometer value with 0.62
+       *  step3 : Display the result
+       *
+       * Note: 1 kilometer = 0.62 miles
        */
 
-      Scanner userInput = new Scanner(System.in) ;
-      System.out.println("Enter your name sir/mam: ");
-      String name = userInput.next();
-      System.out.println("Hello " + name + ", have a good day");
+      Scanner input = new Scanner(System.in) ;
+      System.out.print("Kilometer: ");
+      double kilometer = input.nextDouble() ;
+
+      double miles = kilometer * 0.62 ;
+      System.out.println(miles + " mi");
+
+
 
     }
 }
