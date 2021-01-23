64a236cbba1b498e751f59ddb6a22e3643e99392
faisal <faisal25march99@gmail.com>
2021-01-23 10:56:13 +0530

    Taking input from user, Scanner Class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      // Literals in java
-      byte age = 34 ;
       /*
-      * Here 34 is our Literal
-      * */
-        char ASCII_ch = 90 ;
-        char  ch = 'A';
-        float f1 = 5.6f ;
-        double d1 = 4.53D ;
-        long dinoAge = 5666666666666L ;
-        System.out.println(age);
-        System.out.println(ch);
-
-        String name = "Faisal" ;
-        System.out.println(name);
+       * Taking input from user
+       * Store the value in a variable
+       */
+      Scanner input = new Scanner(System.in) ; // Scanner class
+      System.out.println("Enter the age of user");
+      byte number = input.nextByte() ; // 25
+      System.out.println("The age of user " + number); // 25
     }
 }
 
