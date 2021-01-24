f114abd9d79effe2756b988f50df36b3bf9a8172
faisal <faisal25march99@gmail.com>
2021-01-24 13:19:10 +0530

    Done with Quiz

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Data Type of Expressions & Increment / Decrement Operator
-        /*
-        1. byte + short -> int
-        2. short + int -> int
-        3. long + float -> float
-        4. int + float -> float
-        5. char + int -> int
-        6. char + short -> int
-        7. long + double -> double
-        8. float + double -> double
-         */
-        float a = 6.54f + 6 ;
-        System.out.println(a);
+       int y = 7 ;
+       int x = ++y * 8 ;
+        System.out.println(x); // 64
 
-        // Increment and Decrement Operator
-        int i = 56;
-        System.out.println(i ++); // 56
-        System.out.println(i); // 57
-        System.out.println(++ i); // 58
-        System.out.println(i --); // 58
-        System.out.println(i); // 57
-        System.out.println(-- i); // 56
-
+        char a = 'B';
+        System.out.println(++a); // C
 
     }
 }
