91ac8cba8a80e3760456876e586c84782c484d8b
faisal <faisal25march99@gmail.com>
2021-01-24 13:29:55 +0530

    Done with Ques 2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Question 1
-      float a = (7 / 4) * (9 / 2) ;
-        System.out.println(a);
+      // Question 2
+        /*
+        WAP to encrypt a grade by adding 8 to it. Decrypt it
+        to show the correct grade
 
+         */
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Please enter your grade :");
+        char grade = sc.next().charAt(0) ;
+        grade += 8 ; // encryption
+        System.out.println("Encrypted Grade: " + grade );
+        grade -= 8 ; // decryption
+        System.out.println("Decrypted Grade: " + grade);
     }
 }
 
