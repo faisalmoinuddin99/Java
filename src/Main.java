6f3aaccfd5fc03ccbc574ad1bc4f82076ace378d
faisal <faisal25march99@gmail.com>
2021-01-24 13:23:00 +0530

    Done with Ques 1

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       int y = 7 ;
-       int x = ++y * 8 ;
-        System.out.println(x); // 64
-
-        char a = 'B';
-        System.out.println(++a); // C
+        // Question 1
+      float a = (7 / 4) * (9 / 2) ;
+        System.out.println(a);
 
     }
 }
