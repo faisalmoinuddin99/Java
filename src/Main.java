0c2c3ba564c7b56db22735860ac35c78ba43473f
faisal <faisal25march99@gmail.com>
2021-01-25 05:17:06 +0530

    next() and nextLine()

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-      // Strings Chapter 3
-        /*
-        Different ways to print the values
-         */
-        byte age = 21 ;
-        String name = "Faisal" ;
-        System.out.printf("Name is %s and his age is %d",name, age);
-
-        /*
-        Here %d is Format Specifier
-        %s format specifier for string
-         */
-
-        double salary = 2.50 ;
-        System.out.format("The salary is %f", salary);
+     Scanner sc = new Scanner(System.in) ;
+     String st = sc.next() ;
+     String fullLength = sc.nextLine() ;
+        System.out.println(st);
+        System.out.println(fullLength);
     }
 }
 
