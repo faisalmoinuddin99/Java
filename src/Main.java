40685fb3f04268f7bf9e8fa10d8e2e4bd57584bb
faisal <faisal25march99@gmail.com>
2021-01-26 05:04:13 +0530

    Relational and Logical Operator

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,51 @@ public class Main {
         /*
         Chapter 4 :
 
-        Conditionals - if else statement
-                       switch statement
-         */
-        int age = 64 ;
-        boolean condition = age > 18;
+        Relational and Logical Operator
 
-        System.out.println(condition ? "yes boy you can drive" : "No boy you cant drive yet !");
+        ==, >=, <=, >, <, !=
+
+        &&, ||, !
+         */
+
+        // AND Operator ( && )
+        boolean x1 = true ;
+        boolean y1 = false ;
+
+        boolean x2 = true ;
+        boolean y2 = true ;
+
+        boolean x3 = false ;
+        boolean y3 = true ;
+
+        boolean x4 = false ;
+        boolean y4 = false ;
+
+        System.out.println(x1 && y1); // false
+        System.out.println(x2 && y2); // true
+        System.out.println(x3 && y3); // false
+        System.out.println(x4 && y4); // false
+
+//         OR Opeator ( || )
+
+        System.out.println(x1 || y1); // true
+        System.out.println(x2 || y2); // true
+        System.out.println(x3 || y3); // true
+        System.out.println(x4 || y4); // false
+
+        // NOT Operator (!)
+
+        System.out.println(!x1); // false
+        System.out.println(!x2); // false
+        System.out.println(!x3);// true
+        System.out.println(!x4); // true
+
+        System.out.println(!y1); // true
+        System.out.println(!y2); // true
+        System.out.println(!y3); // false
+        System.out.println(!y4); // true
+
+
     }
 }
 
