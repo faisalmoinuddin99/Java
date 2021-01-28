b536c9f8745df14e83261f582e645c82fcd57fa0
faisal <faisal25march99@gmail.com>
2021-01-28 06:56:40 +0530

    Practice set Question 1

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       /*
-       switch control Instruction
-
-       used to give user a choice
-        */
-
-        Scanner sc = new Scanner(System.in) ;
-        System.out.print("Enter your age:");
-        byte age = sc.nextByte();
-
-        switch (age){
-            case 18 :
-                System.out.println("You are becoming adult");
-                break;
-            case 23 :
-                System.out.println("Got your first job");
-                break ;
-            case 30 :
-                System.out.println("marrying someone");
-                break ;
-            case 35 :
-                System.out.println("becoming father");
-                break;
-            default:
-                System.out.println("Live life happily");
+     /*
+     * Practice set question chapter - 4
+     * */
+        int a = 10 ;
+        if (a = 11) {
+            System.out.println("I am 11");
+        }else {
+            System.out.println("I am 10");
         }
-        System.out.println("Thanks for using my java code");
-
         /*
-        Enhance Switch
-
-        switch(age) {
-        case 18 -> {
-         System.out.println("Got your first job");
-         System.out.println("Driving");
-        }
-        case 23 -> System.out.println("Got your first date");
-        case 60 -> System.out.println("Got your retired");
-        default -> System.out.println("Enjoy your life);
-
-
-        }
-         */
+        * Output : java: incompatible types: int cannot be converted to boolean
+        * */
     }
 }
 
