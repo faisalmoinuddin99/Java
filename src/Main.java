6ec1a1803c839e9b77f6a0d96ca56fec1cf9ba57
faisal <faisal25march99@gmail.com>
2021-01-30 18:41:50 +0530

    Done with sum of first n even number

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,40 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 /*
 Practice Set 5
+
+TODO:
+    sum of first n even number:
+    even number = 0, 2, 4, 6, 8, ....
+    sum = 2+4+6+8 = 20
  */
-        for (int i = 4; i != 0; i--){
-            for(int j = i; j > 0; j--){
-                System.out.print("*");
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("Enter the number:");
+        int number = sc.nextInt() ;
+        int evenNumber = 0;
+        int sum = 0 ;
+        System.out.println("Even Number:");
+        for (int i = 0; i < number; i++){
+            if ( i % 2 == 0 ){
+                evenNumber =  i;
+                System.out.print(evenNumber + " ");
+                sum += evenNumber ;
 
             }
-            System.out.print("\n");
         }
+        System.out.print("\n");
+        System.out.println("Sum: " + sum);
     }
 }
 
 /*
-OUTPUT: Pattern
-****
-***
-**
-*
+OUTPUT:
+
+Enter the number: 10
+Even Number:
+0 2 4 6 8
+Sum: 20
+
  */
