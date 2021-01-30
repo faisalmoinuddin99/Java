f42fca7975fa85d38689232011d260637b992e2f
faisal <faisal25march99@gmail.com>
2021-01-30 11:40:28 +0530

    Done with Question 1 Pattern Problem

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,23 @@
 
 public class Main {
     public static void main(String[] args) {
+/*
+Practice Set 5
+ */
+        for (int i = 4; i != 0; i--){
+            for(int j = i; j > 0; j--){
+                System.out.print("*");
 
-        // Break and Continue using loop
-        for(int i = 0; i<=5; i++) {
-           if(i == 2) {
-               System.out.println("Ending loop");
-               continue;
-           }
-            System.out.println(i);
-            System.out.println("Java is great");
+            }
+            System.out.print("\n");
         }
-
     }
 }
 
 /*
-OUTPUT:
-0
-Java is great
-1
-Java is great
-Ending loop
-3
-Java is great
-4
-Java is great
-5
-Java is great
+OUTPUT: Pattern
+****
+***
+**
+*
  */
-
