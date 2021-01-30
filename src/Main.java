4475d9d8c923cb1d36cba52f0ff382f102619059
faisal <faisal25march99@gmail.com>
2021-01-30 11:28:48 +0530

    Continue in Java

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,30 @@ public class Main {
     public static void main(String[] args) {
 
         // Break and Continue using loop
-        for(int i = 0; i<=5; i++){
-            System.out.println("Inside for loop "+i);
-           if (i == 2){
-               System.out.println("Inside if statement "+i);
-               System.out.println("Ending the loop");
-               break;
+        for(int i = 0; i<=5; i++) {
+           if(i == 2) {
+               System.out.println("Ending loop");
+               continue;
            }
+            System.out.println(i);
+            System.out.println("Java is great");
         }
+
     }
 }
 
 /*
 OUTPUT:
-Inside for loop 0
-Inside for loop 1
-Inside for loop 2
-Inside if statement 2
-Ending the loop
+0
+Java is great
+1
+Java is great
+Ending loop
+3
+Java is great
+4
+Java is great
+5
+Java is great
  */
 
