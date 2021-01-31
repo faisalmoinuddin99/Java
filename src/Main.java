78ea0016ecff31124e24617d69d688f89f36d553
faisal <faisal25march99@gmail.com>
2021-01-31 12:56:54 +0530

    2D Array

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    /*
+    2-D Array
+     */
+        int [][] flats ;
+        int rows = 3;
+        int col = 2 ;
+        flats = new int[2][3];
+        // 2 columns and 3 rows
+        flats[0][0] = 101 ;
+        flats[0][1] = 102 ;
+        flats[0][2] = 103 ;
+        flats[1][0] = 201 ;
+        flats[1][1] = 202 ;
+        flats[1][2] = 203 ;
 
-       /*
-     Quiz:  Traversing array element in reverse order
-        */
-
-        int[] marks = {10,20,30,405,60,708,45,34} ;
 
 
-        System.out.print("\n");
-        System.out.print("For each loop: ");
-        for(int element : marks){
-            System.out.print(element + " ");
+        int len = flats.length;
+        System.out.println(len);
+        for(int i = 0; i < flats.length; i++){
+            for (int j = 0; j < flats[i].length; j++){
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
         }
+
+
     }
 }
 
+/*
+
+101 102 103
+201 202 203
+
+ */
