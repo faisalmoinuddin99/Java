9b17f52677180af18ff88502d4d757584892ccb8
faisal <faisal25march99@gmail.com>
2021-01-31 12:25:27 +0530

    for Each loop

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,12 @@ public class Main {
         */
 
         int[] marks = {10,20,30,405,60,708,45,34} ;
-        for (int  i = marks.length - 1; i>=0; i--){
-            System.out.print(marks[i] + " ");
+
+
+        System.out.print("\n");
+        System.out.print("For each loop: ");
+        for(int element : marks){
+            System.out.print(element + " ");
         }
     }
 }
