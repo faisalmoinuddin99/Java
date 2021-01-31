4d520697f9e18c09d910e666ee04df2813d18b51
faisal <faisal25march99@gmail.com>
2021-02-01 05:02:49 +0530

    Done with Problem 6

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     /*
-   Reverse an array
+        find max element present in the array
      */
-int [] arr = {1,2,3,4,5, 21, 78};
-int l = arr.length;
-int n = Math.floorDiv(l, 2); // find mid element with greatest integer
-int temp = 0;
-for(int i = 0; i < n; i++){
-    // swap arr[i] arr[l-1-i]
-    temp = arr[i];
-    arr[i] = arr[l-1-i];
-    arr[l-1-i] = temp ;
 
-}
-for(int  element : arr){
-    System.out.print(element+ " ");
-}
+        int[] arr = {10, 5, 20, 3,7,90};
+        int len = arr.length;
+        int max = 0;
 
+        for (int i = 0; i < len; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.print("Max "+ max);
     }
 }
 
 /*
-OUTPUT:  78 21 5 4 3 2 1
+OUTPUT:  Max 90
+
 
 
  */
