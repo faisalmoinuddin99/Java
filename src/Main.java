bfe78c0fdd41e16369d9ef0513ff1b372ada371f
faisal <faisal25march99@gmail.com>
2021-01-31 11:59:05 +0530

    Some more demo of Arrays

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-/*
-Chapter 6:  Arrays is a collection of similar type of data
- */
-    // Declare + initialize
-        int[] marks = {50,40,30,51,60} ;
-        int len = marks.length;
+
+        // find array length
+        int [] marks = {90,78,56,8,65,45};
+        int len = marks.length ;
+        System.out.println(marks[1]);
         System.out.println(len);
 
-    }
+        // floating point array
+        float[] marks1 = {90.5f,78.78f,56.09f,8.7f,65.9f,45.6f};
+        System.out.println(marks1[3]);
 
+        //string array
+        String[] students = {"faisal", "harry", "rohan", "shubham"};
+        int studentLength = students.length;
+        System.out.println(studentLength);
+        System.out.println(students[2]);
+    }
 }
 
