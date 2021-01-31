f11232f2f499874a4857f5b9504d6ca8728353b2
faisal <faisal25march99@gmail.com>
2021-01-31 15:23:01 +0530

    Done with problem 1

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     /*
-    2-D Array
+    Create an array of 5 floats and calculate the sum
      */
-        int [][] flats ;
-        int rows = 3;
-        int col = 2 ;
-        flats = new int[2][3];
-        // 2 columns and 3 rows
-        flats[0][0] = 101 ;
-        flats[0][1] = 102 ;
-        flats[0][2] = 103 ;
-        flats[1][0] = 201 ;
-        flats[1][1] = 202 ;
-        flats[1][2] = 203 ;
-
-
-
-        int len = flats.length;
-        System.out.println(len);
-        for(int i = 0; i < flats.length; i++){
-            for (int j = 0; j < flats[i].length; j++){
-                System.out.print(flats[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
-
-
+    float [] myArray = {1.0f,2.1f,3.5f,4.7f,5.8f};
+    float sum = 0f;
+    int length = myArray.length;
+    int i = 0;
+    while(i < length){
+        sum = sum + myArray[i];
+        i++;
+    }
+        System.out.format("The sum of array is %.2f", sum);
     }
 }
 
 /*
-
-101 102 103
-201 202 203
-
+OUTPUT: The sum of array is 17.10
  */
