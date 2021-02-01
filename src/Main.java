b06966cb465b906da22584f362fdaf8f99bc2b14
faisal <faisal25march99@gmail.com>
2021-02-02 05:11:46 +0530

    Difference between Arguments and Parameter

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,31 @@ import java.util.Scanner;
 
 public class Main {
 
+    /*
+    Method Overloading
 
+    . Two or more methods can have same name but different parameters.
+    .  Ek he naam but kaam alag alag
+     */
+    static void foo(){
+        System.out.println("I drive a car");
+    }
 
-     static void changeVarValue(int a){
-         a = 89;
-     }
-
-     static void changeArrValue(int[] a) {
-         a[0] = 90;
-     }
+    static void foo(int x){
+        System.out.println("I had fly " + x + " planes");
+    }
 
     public static void main(String[] args) {
 
-    // Case 1:
-    int x = 10;
-    changeVarValue(x);
-    System.out.println("After passing x to the change Variable value: " + x); // 10
-
-    // Case 2:
-    int [] arr = {10,20,30,50} ;
-    changeArrValue(arr);
-        System.out.println("After passing arr to the change Array value: " + arr[0]); // 90
-
-
-        /*
-        Case 1: In this case, value dose not  change because the copy is passed.
-
-        Case 2: In case of Array, the reference is passed. Hence arr in main method and a in changeArrValue method
-        are pointing to same reference in memory location [ [I@edf4efb ].
-         */
+    foo(); // I drive a car
+    foo(10); // I had fly 10 planes
 
     }
 }
 
 /*
-OUTPUT:
 
-
-
-
-
-
+Arguments are actual
+. eg foo(10) -> here 10 is an argument
+. eg foo(int x) -> here int x is a parameter
  */
