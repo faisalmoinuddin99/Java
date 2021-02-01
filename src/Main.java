8f71937789ca3d084e17118ea6a5351cc091fa6d
faisal <faisal25march99@gmail.com>
2021-02-02 05:20:56 +0530

    Method Overloading with example and with error example as well

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,27 @@ public class Main {
         System.out.println("I drive a car");
     }
 
+    // Error : method foo() is already defined in class Main
+    static String foo(){
+        return  "I drive a car" ;
+    }
+    /*
+    Note : Method overloading cannot be performed by changing he return type of the method.
+     */
+
     static void foo(int x){
         System.out.println("I had fly " + x + " planes");
+    }
+
+    static void foo(String name, int x){
+        System.out.println("Love you " +  name + " " + x);
     }
 
     public static void main(String[] args) {
 
     foo(); // I drive a car
     foo(10); // I had fly 10 planes
-
+    foo("Ben",3000); // Love you Ben 3000
     }
 }
 
