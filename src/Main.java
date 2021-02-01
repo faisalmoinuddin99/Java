0eea6fa804d9e02d6a44cc06b533ab67a879e9f3
faisal <faisal25march99@gmail.com>
2021-02-02 04:22:12 +0530

    Static keyword with example of method

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
     /*
-    Introduction to Methods
+    With Static keyword
+
+    Theory: static keyword helps you to create and access the
+    methods without creating any object of the class.
+
+    . Which means you can directly call the method in main
+    method.:
      */
-        System.out.println("Chapter 7 ---- Methods");
+
+    static int addition(int x, int y){
+        return  x + y ;
+    }
+
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = addition(a,b);
+        System.out.println(c); // 30
+
+
     }
 }
 
