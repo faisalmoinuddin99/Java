181c110604ac897fb07c5de0459ae786e8d11417
faisal <faisal25march99@gmail.com>
2021-02-02 04:58:15 +0530

    Trying to change the value of variable and array, Simple variable cannot be change because copy of the reference is passed. But in case of Array actual reference is passed

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    /*
-    Without Static keyword
 
-    Theory: if you don't make your methods with static keyword, then for accessing
-    that method in main you have to create an object of your class and then you can
-    call that method.
-     */
 
-     int addition(int x, int y){
-        return  x + y ;
-    }
+     static void changeVarValue(int a){
+         a = 89;
+     }
+
+     static void changeArrValue(int[] a) {
+         a[0] = 90;
+     }
 
     public static void main(String[] args) {
-    int a = 10;
-    int b = 20;
-    Main obj = new Main();
-    int c = obj.addition(a,b);
-        System.out.println(c); // 30
 
+    // Case 1:
+    int x = 10;
+    changeVarValue(x);
+    System.out.println("After passing x to the change Variable value: " + x); // 10
+
+    // Case 2:
+    int [] arr = {10,20,30,50} ;
+    changeArrValue(arr);
+        System.out.println("After passing arr to the change Array value: " + arr[0]); // 90
+
+
+        /*
+        Case 1: In this case, value dose not  change because the copy is passed.
+
+        Case 2: In case of Array, the reference is passed. Hence arr in main method and a in changeArrValue method
+        are pointing to same reference in memory location [ [I@edf4efb ].
+         */
 
     }
 }
