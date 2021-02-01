474a02dfe7d1198958a72b1434da9b74d17ec097
faisal <faisal25march99@gmail.com>
2021-02-01 10:50:10 +0530

    Introduction to Methods Chapter 7

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     /*
-       check whether the array is sorted or not
+    Introduction to Methods
      */
-//    int[] array = {1, 2100, 3, 455, 5, 34, 67} ;
-        int[] array = {1, 2, 3, 4, 5, 34, 67} ;
-    boolean isSorted = true;
-    for (int i = 0; i < array.length - 1; i++){
-        if (array[i] > array[i+1]) {
-            isSorted = false ;
-        }
-    }
-    if (isSorted){
-        System.out.println("Yes, Sorted");
-    } else {
-        System.out.println("No, not Sorted");
-    }
-
+        System.out.println("Chapter 7 ---- Methods");
     }
 }
 
 /*
-OUTPUT:  Yes, Sorted
+OUTPUT:
+
 
 
 
