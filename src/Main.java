3fe48d42853ccc11ce997ec683d5c44d2cda9893
faisal <faisal25march99@gmail.com>
2021-02-03 05:21:41 +0530

    Introduction to Recursion

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    /*
-        Variable Argument (VarArgs)
-
-     */
-//    static int sum(int a, int b) {
-//        return a + b;
-//    }
-//
-//    static int sum(int a, int b, int c){
-//        return a + b + c ;
-//    }
-//
-//    static int sum(int a, int b, int c, int d){
-//        return a + b + c + d ;
-//    }
-    /*
-    Instead of creating lots of overloaded function, you can use VarArgs, refer below example
-     */
-
-    static int sum(int x,int ...arg){
-        // now here x is compulsory Parameter, user have to pass argument for parameter x
-        int result = x;
-        for(int a : arg){
-            result += a;
-        }
-        return  result ;
-    }
     public static void main(String[] args) {
-
-//        System.out.println("Sum is: " + sum()); // error
-        System.out.println("compulsory Parameter "+ sum(2)); // compulsory Parameter 2
-        System.out.println("Sum of 1 and 2 is: " + sum(1,2));
-        System.out.println("Sum of 1, 2 and 3 is: " + sum(1,2,3));
-        System.out.println("Sum of 1, 2, 3, and 4 is: " + sum(1,2,3,4));
+        System.out.println("Introduction to Recursion");
+    }
     }
 
-    }
+
 
 
 /*
-OUTPUT:
-Sum of 1 and 2 is: 3
-Sum of 1, 2 and 3 is: 6
-Sum of 1, 2, 3, and 4 is: 10
+
 
  */
