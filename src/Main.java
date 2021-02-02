1efd47b429bd63f0eaa1484cefb94bb5b2c93553
faisal <faisal25march99@gmail.com>
2021-02-03 05:04:47 +0530

    what if I give empty method ?

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ public class Main {
     }
     public static void main(String[] args) {
 
+        System.out.println("Sum is: " + sum()); // 0
         System.out.println("Sum of 1 and 2 is: " + sum(1,2));
         System.out.println("Sum of 1, 2 and 3 is: " + sum(1,2,3));
         System.out.println("Sum of 1, 2, 3, and 4 is: " + sum(1,2,3,4));
