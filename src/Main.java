34bef1d5dfe5c9d3f33522dc13e616cdb3bed182
faisal <faisal25march99@gmail.com>
2021-02-03 17:46:40 +0530

    Chapter 7 Practice Problem 1 -- Multiplication Table using method

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
+    /*
+    WAP method to print multiplication table of a
+    number n
+     */
 
-    static int Factorial(int n){
-        if (n == 0 || n == 1){
-            return 1;
-        } else {
-            return n * Factorial(n-1);
+    static void multiplicationTable(int n, int till){
+        int table = 1;
+        for(int i = 1; i<=till; i++){
+        table = n * i;
+            System.out.format("%d * %d = %d ",n,i,table);
+            System.out.print("\n");
         }
-    }
 
-    static int Factorial_Iterative(int n){
-        int product = 1;
-        if (n==0 || n==1){
-            return 1;
-        }else{
-
-            for (int i = 1; i <= n; i++){
-                product *= i;
-            }
-        }
-        return product ;
     }
     public static void main(String[] args) {
-        /*
-        Factorial(5) = 5 * 4 * 3 * 2 * 1
-        Factorial(n) = n * n - 1 ... 2 * 1
-
-        Formula : n * Factorial(n-1)
-         */
-        Scanner sc = new Scanner(System.in) ;
-        System.out.print("Enter the number :");
-        int input = sc.nextInt() ;
-
-        System.out.print("Factorial : " + Factorial(input));
-        System.out.print("\n");
-        System.out.print("Factorial_Iterative: " + Factorial_Iterative(input) );
+    multiplicationTable(4,10);
     }
 
 }
-
-
-
 
 /*
 
 OUTPUT :
 
-Enter the number :5
-Factorial : 120
-Factorial_Iterative: 120
+4 * 1 = 4
+4 * 2 = 8
+4 * 3 = 12
+4 * 4 = 16
+4 * 5 = 20
+4 * 6 = 24
+4 * 7 = 28
+4 * 8 = 32
+4 * 9 = 36
+4 * 10 = 40
+
  */
