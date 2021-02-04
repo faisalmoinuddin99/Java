075a3c0989ae78a93696ae783382179ce07d0be7
faisal <faisal25march99@gmail.com>
2021-02-04 07:18:04 +0530

    Fibonnaci Series Part B

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,15 @@ public class Main {
 
      */
     static  int fib(int n) {
-        if (n==1){
-            return 0;
-        }else if(n==2){
-            return 1;
-        }else {
+//        if (n==1){
+//            return 0;
+//        }else if(n==2){
+//            return 1;
+//        }
+        if (n==1 || n==2){
+            return n-1; // here if n = 1 then 1-1 = 0 OR n = 2 then n-1 = 2 - 1 = 1
+        }
+        else {
             return fib(n-1) + fib(n-2);
         }
     }
