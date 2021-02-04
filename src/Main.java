0d0da1505f8f13c421cc830ac1ae33689e34e763
faisal <faisal25march99@gmail.com>
2021-02-04 07:02:13 +0530

    One more method of sum of n using recursion

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,21 @@ static int sum(int n){
 
 }
 
+static int sumRec(int n){
+    if(n == 1){
+        return 1;
+    }else{
+        return n + sum(n-1);
+    }
+}
+
 
     public static void main(String[] args) {
     int x = sum(3);
         System.out.println("Sum : " + x);
+
+        int y = sumRec(4);
+        System.out.println("Sum Rec: " + y);
     }
 
 }
