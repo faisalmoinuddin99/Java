50255abb5a35df17db824cef9bbf43f5d0c5000b
faisal <faisal25march99@gmail.com>
2021-02-05 06:57:01 +0530

    Defination of Oops

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        Object Oriented Programming tires to map code instruction with real world making the code short
+        and easier to understand.
+
+        WHAT IS OBJECT ORIENTED PROGRAMMING ?
+        -> Solving the problem by creating objects is one of the most popular approaches in programming .
+        This is called Object Oriented Programming.
+
+         */
         System.out.println("Introduction to OOPs");
 
     }
