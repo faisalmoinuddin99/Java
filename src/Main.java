0c240cf4c6fb0788c7dacc54a00e240e2771577c
faisal <faisal25march99@gmail.com>
2021-02-07 10:24:15 +0530

    Create your own Java Class

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,47 @@
-import java.util.Scanner;
 
+
+ class Employee {
+    int id ;
+    String name ;
+    int salary;
+
+    public void printDetails(){
+        System.out.println("My id number is : " + id);
+        System.out.println("My name is : " + name);
+    }
+    public void getSalary(){
+        System.out.format("%s salary is %dk",name,salary);
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Custom Class ");
+        Employee faisal = new Employee(); // Instantiating a new Employee Object
+        Employee jhon = new Employee() ;
+        // Setting Attribute
+        faisal.id = 124;
+        faisal.name = "Faisal";
+
+        jhon.id = 13;
+        jhon.name = "Jhon Khandelwal" ;
+        jhon.salary = 50 ;
+        // Printing Attribute
+//        System.out.println(faisal.id + " " + faisal.name );
+        faisal.printDetails();
+        jhon.printDetails();
+        jhon.getSalary();
+
         /*
-
-       OOPS Terminology :
-
-       1. Abstraction : Hiding Internal Information [ Show only essential info ]
-
-       2. Encapsulation : The act of putting various component together [ just like a capsule ]
-
-       example :- Laptop is a single entity with wifi + speaker  + storage in a single box !
-
-       In java, Encapsulation simply means that the sensitive information / data can be hidden from
-       the user.
-
-        */
-        System.out.println("Introduction to OOPs");
+        Custom Class
+My id number is : 124
+My name is : Faisal
+My id number is : 13
+My name is : Jhon Khandelwal
+Jhon Khandelwal salary is 50k
+         */
 
     }
 
 }
-
-/*
-
-OUTPUT :
-
-*
-**
-***
-****
-
-
- */
