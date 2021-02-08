c0086d4ee5c2fa70a23f905fccf7da10fc663278
faisal <faisal25march99@gmail.com>
2021-02-09 04:43:20 +0530

    Done with Problem 2

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,25 @@
  /*
  Practice Set
   */
- class Employee {
-     // properties
-     String name;
-     int salary ;
 
-     // methods
+class CellPhone {
 
-     public String getName() {
-         return name;
-     }
+    public  void ring(){
+        System.out.println("ringing");
+    }
 
-     public void setName(String name) {
-         this.name = name;
-     }
+    public void vibrate(){
+        System.out.println("Vibrating");
+    }
+}
 
-     public int getSalary(){
-         return salary ;
-     }
- }
 public class Main {
 
     public static void main(String[] args) {
 
-Employee e1 = new Employee() ;
-        e1.name = "Faisal";
-        e1.salary = 500000 ;
-        System.out.println(e1.name);
-        System.out.println(e1.getSalary());
-        Employee e2 = new Employee() ;
-        e2.setName("Rahul");
-        System.out.println(e2.getName());
-        System.out.println(e2.getSalary());
+    CellPhone c1 = new CellPhone();
+    c1.ring();
+    c1.vibrate();
     }
 
 }
