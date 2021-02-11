34edbead277b305ec8a1e2fcaa6c755de7242f58
faisal <faisal25march99@gmail.com>
2021-02-11 07:19:00 +0530

    Java Parameterized Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
-class Employee {
+/*
+Java Parameterized Constructor
+A constructor which has a specific number of parameters is called a parameterized constructor.
 
-    public Employee(int ...args){
-        System.out.println("Printing the Id's");
-        int findNoOfEmployee = 0;
-        for (int i : args){
-            System.out.print(i + " ");
-            findNoOfEmployee ++;
-        }
-        System.out.print("\n");
-        System.out.println("Number of Employee present in a company : " + findNoOfEmployee);
+Why use the parameterized constructor?
+The parameterized constructor is used to provide different values to distinct objects.
+However, you can provide the same values also.
+
+Example of parameterized constructor
+In this example, we have created the constructor of Student class that have two parameters. We can have any number of parameters in the constructor.
+ */
+
+class Students {
+    int id ;
+    String name ;
+
+    public Students(int id, String name) {
+        this.id = id;
+        this.name = name;
+//        System.out.println(this.id + " " + this.name);
     }
+
+    public void displayStudentsDetails(){
+        System.out.println(this.id + " " + this.name);
+    }
+
 }
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee(1,2,3,4,5,6,7,8,9,10);
+        Students s1 = new Students(1,"faisal" );
+        Students s2 = new Students(2,"Rahul") ;
 
+        s1.displayStudentsDetails();
+        s2.displayStudentsDetails();
+
+        /*
+        1 faisal
+        2 Rahul
+         */
     }
 }
