6438ce04843c08bfe80cf7c480eb683d8664218f
faisal <faisal25march99@gmail.com>
2021-02-11 10:17:11 +0530

    DSA Problem 1

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,42 @@
-/*
-Java Parameterized Constructor
-A constructor which has a specific number of parameters is called a parameterized constructor.
-
-Why use the parameterized constructor?
-The parameterized constructor is used to provide different values to distinct objects.
-However, you can provide the same values also.
-
-Example of parameterized constructor
-In this example, we have created the constructor of Student class that have two parameters. We can have any number of parameters in the constructor.
- */
-
-class Students {
-    int id ;
-    String name ;
-
-    public Students(int id, String name) {
-        this.id = id;
-        this.name = name;
-//        System.out.println(this.id + " " + this.name);
-    }
-
-    public void displayStudentsDetails(){
-        System.out.println(this.id + " " + this.name);
-    }
-
-}
-
 public class Main {
     public static void main(String[] args) {
-        Students s1 = new Students(1,"faisal" );
-        Students s2 = new Students(2,"Rahul") ;
+        String [] arr = {"abc", "xyz", "uvw", "efg"};
+        int length = arr.length;
+        int start = 0;
+        int end = length - 1;
+        System.out.println("Before Reverse: ");
 
-        s1.displayStudentsDetails();
-        s2.displayStudentsDetails();
+        printArray(arr);
 
-        /*
-        1 faisal
-        2 Rahul
-         */
+        System.out.println();
+        System.out.println("After Reverse: ");
+
+        ReversingArray(arr,start,end);
+    }
+
+    public static void printArray(String[] a){
+        for (String i : a){
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void ReversingArray(String[] a, int start, int end){
+        String temp ;
+        int size = a.length;
+        while(start < end){
+            temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;
+            end--;
+        }
+        printArray(a);
     }
 }
+
+/*
+Before Reverse:
+abc xyz uvw efg
+After Reverse
+efg uvw xyz abc
+ */
