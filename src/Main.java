5db10b50459bd816a92bc79403ba38d190fa9b47
faisal <faisal25march99@gmail.com>
2021-02-12 07:39:51 +0530

    Problem Statement

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        String [] arr = {"abc", "xyz", "uvw", "efg"};
-        int length = arr.length;
-        int start = 0;
-        int end = length - 1;
-        System.out.println("Before Reverse: ");
-
-        printArray(arr);
-
-        System.out.println();
-        System.out.println("After Reverse: ");
-
-        ReversingArray(arr,start,end);
-    }
-
-    public static void printArray(String[] a){
-        for (String i : a){
-            System.out.print(i + " ");
-        }
-    }
-
-    public static void ReversingArray(String[] a, int start, int end){
-        String temp ;
-        int size = a.length;
-        while(start < end){
-            temp = a[start];
-            a[start] = a[end];
-            a[end] = temp;
-            start++;
-            end--;
-        }
-        printArray(a);
-    }
-}
-
 /*
-Before Reverse:
-abc xyz uvw efg
-After Reverse
-efg uvw xyz abc
+GUESS THE NUMBER GAME :
+
+
+Create a class game, which allows a user to play " Guess the Number "
+game once. Game should have a following methods :
+
+1. Constructor to generate the random number.
+2. takeUserInput() to take a user input of a number
+3. isCorrectNumber() to detect whether the number entered by the
+   user is true.
+4. getter and setter for noOfGuesses
+
+use properties such as noOfGuesses(int), etc to get this task done !
  */
