65f2b24d109105737857c8319da0e3a5e8f9def4
faisal <faisal25march99@gmail.com>
2021-02-17 07:40:11 +0530

    Inheritance Example extends keyword

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,43 @@
 INHERITANCE - CHAPTER - 10
  */
 
+class Base {
+
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void display(){
+        System.out.println("I am Base class");
+    }
+}
+
+class Derived extends Base {
+    int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
+
 public class  Main {
+
     public static void main(String[] args) {
-        System.out.println("Chapter - 10 INHERITANCE ");
+       Derived d1 = new Derived() ;
+       d1.setX(60);
+        System.out.println("Value of X " + d1.getX()); // Value of X 60
+        d1.display(); // I am Base class
+        d1.setY(100);
+        System.out.println("Value of Y " + d1.getY()); // Value of Y 100
     }
 }
