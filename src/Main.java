f6221c3f861989c81c48b1ab86420d49e14c1e1a
faisal <faisal25march99@gmail.com>
2021-02-18 07:20:18 +0530

    Constructor Overloading in Inheritance Example 2

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,45 @@
-class Base {
-    public Base() {
-        System.out.println("I am constructor");
+class GrandParent {
+    int land ;
+    public GrandParent() {
+        System.out.println("25 acer land");
+    }
+    public GrandParent(int son ){
+       land =  25 / son ;
+        System.out.println("Overloaded GrandParent Constructor " +
+                "Divide the land : " + land);
     }
 }
 
-class Derived extends Base {
-    public Derived() {
-        System.out.println("I am derived constructor");
+class Parent extends GrandParent{
+   int cars ;
+    public Parent() {
+        //super(4); // Calling the Overloaded Constructor
+        /*
+        if super() is empty then its call the normal constructor
+         */
+        System.out.println("40 Cars");
+    }
+    public Parent(int children){
+        super(4);
+        cars = 40 / children ;
+        System.out.println("Parent Overloaded Constructor " +
+                "Car is divided equally to all the kids " + cars);
     }
 }
+
+class Child extends Parent {
+    public Child() {
+        super(2);
+        System.out.println("I have an iPhone");
+    }
+    public Child(int x, int y){
+        System.out.println("Child's Overloaded Constructor Value of x and y " + x +  " " + y);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Base b1 = new Base(); // I am constructor
-
-        Derived d1 = new Derived();
-        /*
-        I am constructor
-        I am derived constructor
-        */
-
+//        Child c1 = new Child();
+        Child c = new Child(4,5);
     }
 }
