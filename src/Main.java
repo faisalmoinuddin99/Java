468e5519cf97642990bfd7eff2a3e267ad7028e7
faisal <faisal25march99@gmail.com>
2021-02-18 06:51:03 +0530

    example 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+class Base {
+    public Base() {
+        System.out.println("I am constructor");
+    }
+}
+
+class Derived extends Base {
+    public Derived() {
+        System.out.println("I am derived constructor");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Constructor in Inheritance");
+        Base b1 = new Base(); // I am constructor
+
+        Derived d1 = new Derived();
+        /*
+        I am constructor
+        I am derived constructor
+        */
+
     }
 }
