219b6ee4d26f1ac2562c248120814c039d2b58cf
faisal <faisal25march99@gmail.com>
2021-02-22 10:49:08 +0530

    Example 1 of Method Over Riding

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+class A {
+  public void printName() {
+      System.out.println("I am in class A");
+    }
+}
+
+class B extends A {
+
+    @Override
+    public void printName(){
+        System.out.println("I am in class B");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Method Overriding ");
+        A a = new A();
+        a.printName();
+
+        B b = new B();
+        b.printName();
     }
 }
