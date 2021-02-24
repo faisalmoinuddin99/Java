9e643b437bb676fa7aeb6d8fe93c957377569ad8
faisal <faisal25march99@gmail.com>
2021-02-24 06:46:39 +0530

    Dynamic Method Dispatch

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
-class A {
-  public void printName() {
-      System.out.println("I am in class A");
-    }
-}
-
-class B extends A {
-
-    @Override
-    public void printName(){
-        System.out.println("I am in class B");
-    }
-}
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        A a = new A();
-        a.printName();
-
-        B b = new B();
-        b.printName();
+        System.out.println("Intro to Dynamic Method Dispatch");
     }
 }
