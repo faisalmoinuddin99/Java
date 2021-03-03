79f38571baa5fdd8f7d9442290cfc1174254e10e
faisal <faisal25march99@gmail.com>
2021-03-04 05:17:29 +0530

    Some handson example on Interface - Default method, abstract method, implements keyword

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+interface Phone {
+    int price = 50;
+    void openCamera();
+    void clickPhoto();
+}
+
+interface Media {
+    default void onRadio() // Default method
+    {
+        System.out.println("Playing radio");
+    }
+    void watchYouTube(); // abstract method
+}
+
+class Apple implements Phone, Media{
+
+    @Override
+    public void openCamera() {
+        System.out.println("opening camera");
+    }
+
+    @Override
+    public void clickPhoto() {
+        System.out.println("clicked photo");
+    }
+
+
+
+    @Override
+    public void watchYouTube() {
+        System.out.println("Gaming Videos");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Intro to Interfaces");
+        Phone p = new Apple(); // Phone Reference pointing to Apple Object
+        p.clickPhoto(); // clicked photo
+        p.openCamera(); //
+        System.out.println("Phone[interface] ref: "+ p.price); // 50
+
+        Apple iphone11 = new Apple();
+        System.out.println("Apple[class] ref: " + iphone11.price); // 50
+        iphone11.onRadio(); // Playing radio
+        iphone11.watchYouTube(); // Gaming Videos
+
     }
 }
